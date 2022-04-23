d8f9bd22279083de0300e036d62bfb215b31c9d1
tratchapong <tratchapong@gmail.com>
2022-04-23 17:12:13 +0700

    feth data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Posts from "./Posts";
-import data from "./data"
-
+// import data from "./data"
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 function App() {
-  
+  const [data, setData] = useState([])
+
+  useEffect( ()=> {
+    // const getData = async () => {
+    //   let res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => setData(res.data))
+  },[])
+
   return (
     <div className="container mw-800">
-      <Posts data={data}/>
+      <Posts data={data.slice(0,20)}/>
     </div>
   );
 }
