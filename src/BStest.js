5e440e4c2f67bb6f69faf1008b170b1af98ad6a9
tratchapong <tratchapong@gmail.com>
2022-04-23 15:26:24 +0700

    Bootstrap5 with react start here

---
+++ src/BStest.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function BStest() {
+  return (
+    <div className="container text-center">
+      <h1 className="border text-danger">BS5 Test (text in red + border)</h1>
+      <div
+        className="alert alert-warning alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>Try close this</strong>to test if BS5 JS bundle working...
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
+    </div>
+  );
+}
+
+export default BStest;
