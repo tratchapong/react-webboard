4853d6c6cdf0e81184b079e5ea865ef52fd12ac7
tratchapong <tratchapong@gmail.com>
2022-04-24 23:14:33 +0700

    add async-await example

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import axios from 'axios'
 import {useState, useEffect} from 'react'
 
 function App() {
-  const [data, setData] = useState([])
+  const [allPosts, setAllPosts] = useState([])
+  // useEffect( ()=> {
+  //   axios.get('https://jsonplaceholder.typicode.com/posts')
+  //   .then(res => {
+  //     console.log(res.data)
+  //     setAllPosts(res.data)
+  //   })
+  //   .catch( err => {
+  //     console.log(err.toJSON())
+  //   })
+  // },[] )
 
   useEffect( ()=> {
-    // const getData = async () => {
-    //   let res = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(res => setData(res.data))
+    const getData = async () => {
+      try {
+        let res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setAllPosts(res.data)
+      } catch (err) {
+        console.log(err.toJSON())
+      }
+    }
+    getData()
   },[])
 
   return (
-    <div className="container mw-800">
-      <Posts data={data.slice(0,20)}/>
+    <div className="App container mw-800">
+      <h1 className="bg-info border container text-center mb-3">
+        Codecamp11 <i className="bi bi-wordpress" /> 
+      </h1>
+      <Posts data={allPosts.slice(0,20)} />
     </div>
   );
 }
